9ce9ac8f1c2b2e067eb2903ba182c52ed7f8fd65
Hiroaki Nakamura <hnakamur@gmail.com>
2025-03-03 07:30:58 +0900

    Add sudo to shard.sh and no-shard.sh

--- shard.sh
+++ shard.sh
@@ -8,8 +8,8 @@ work_dir=${PWD}/work-shard-$(date +%Y%m%dT%H%M%S)
 mkdir -p ${work_dir}
 
 jenkins_uid=1200
-chown ${jenkins_uid}:${jenkins_uid} ${work_dir}
+sudo chown ${jenkins_uid}:${jenkins_uid} ${work_dir}
 
-seq 0 $(($shardcnt-1)) | parallel --results ${work_dir}/log_{} "docker run -e SHARD={} -e SHARDCNT=${shardcnt}  -e LANG=C --mount type=bind,source=${work_dir},target=/work --rm fedora41autest"
+seq 0 $(($shardcnt-1)) | sudo parallel --results ${work_dir}/log_{} "docker run -e SHARD={} -e SHARDCNT=${shardcnt}  -e LANG=C --mount type=bind,source=${work_dir},target=/work --rm fedora41autest"
 
 echo work_dir=${work_dir}
